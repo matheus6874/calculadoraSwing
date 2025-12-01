94c4cff73ad8b8fc721cc9c7a52c9eea2411a4de
matheus6874 <matheus.scristino@gmail.com>
2025-11-30 22:11:56 -0300

    mapeamento dos botoes

--- calculadoraSwing/src/main/java/org/matheus/calc/modelo/Memoria.java
+++ calculadoraSwing/src/main/java/org/matheus/calc/modelo/Memoria.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Memoria {
 
+    private enum TipoComando{
+        ZERAR, NUMERO, DIVISAO, MULTIPLICACAO, SUBTRACAO, SOMA, IGUAL, VIRGULA
+    }
+
     private static final Memoria instancia = new Memoria();
 
     private final List<MemoriaObservador> observadores = new ArrayList<>();
@@ -27,13 +31,48 @@ public class Memoria {
         return textAtual.isEmpty() ? "0" : textAtual;
     }
 
-    public void processadorComando(String valor){
-        if ("AC".equals(valor)){
+    public void processadorComando(String texto){
+        TipoComando tipoComando = detectarTipoComando(texto);
+
+        if ("AC".equals(texto)){
             textAtual = "";
         }else {
-            textAtual += valor;
+            textAtual += texto;
         }
         observadores.forEach(o -> o.valorAlterado(getTextAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+        if (textAtual.isEmpty() && texto == "0"){
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        }catch (NumberFormatException e){
+            if ("AC".equals(texto)){
+                return TipoComando.ZERAR;
+            }else if ("/".equals(texto)){
+                return TipoComando.DIVISAO;
+            }
+            else if ("*".equals(texto)){
+                return TipoComando.MULTIPLICACAO;
+            }
+            else if ("+".equals(texto)){
+                return TipoComando.SOMA;
+            }
+            else if ("-".equals(texto)){
+                return TipoComando.SUBTRACAO;
+            }
+            else if (",".equals(texto)){
+                return TipoComando.VIRGULA;
+            }
+            else if ("=".equals(texto)){
+                return TipoComando.IGUAL;
+            }
+        }
+        return null;
     }
 
 }
